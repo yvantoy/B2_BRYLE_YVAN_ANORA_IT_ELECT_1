9c3e34c143080fe74b0a39fa09fc1e64174e8959
Anora_B/yvantoy <bryleyvananora@gmail.com>
2025-09-19 12:52:43 +0800

    MidAct1

---
+++ App.js
@@ -0,0 +1,11 @@
+import Messenger from './Messenger'
+import CommentApp from './CommentApp'
+
+export default function App(){
+  return(
+    <div>
+      <Messenger/>
+      <CommentApp/>
+    </div>
+  )
+}
